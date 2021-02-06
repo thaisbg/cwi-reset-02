2282949732420fa46b71b8566714c609daf8ea95
thaisbg <thsbrgc@gmail.com>
2021-02-06 16:04:14 -0200

    Testando segunda atualização

--- meu-projeto/src/Main.java
+++ meu-projeto/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Meu primeiro exercício - reset <3");
+        System.out.println("Meu primeiro exercício - reset edição 02 <3");
         System.out.println("Testando: atualizar arquivo no git");
     }
 }
