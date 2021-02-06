0fda58c827f3d85aa15196ba3fc9206655df9af1
thaisbg <thsbrgc@gmail.com>
2021-02-06 15:49:55 -0200

    Testando primeira atualização no código

--- meu-projeto/src/Main.java
+++ meu-projeto/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Meu primeiro exercício - reset <3");
+        System.out.println("Testando: atualizar arquivo no git");
     }
 }
