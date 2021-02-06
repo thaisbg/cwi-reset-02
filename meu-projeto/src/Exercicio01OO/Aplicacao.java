e1736b1a09810dfe0f3179b2708f5189d2858d48
thaisbg <thsbrgc@gmail.com>
2021-02-06 19:06:47 -0200

    Adicionando exercício 01 de OO

--- meu-projeto/src/Exercicio01OO/Aplicacao.java
+++ meu-projeto/src/Exercicio01OO/Aplicacao.java
@@ -1,4 +1,19 @@
 package Exercicio01OO;
 
 public class Aplicacao {
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int resultadoSoma = calculadora.soma(5, 8);
+        System.out.println("Resultado da soma: " + resultadoSoma);
+
+        int resultadoSubtrai = calculadora.subtrai(12, 6);
+        System.out.println("Resultado da subtração: " + resultadoSubtrai);
+
+        int resultadoMultiplica = calculadora.multiplica(5, 3);
+        System.out.println("Resultado da multiplicação: " + resultadoMultiplica);
+
+        int resultadoDivide = calculadora.divide(14, 2);
+        System.out.println("Resultado da divisão: " + resultadoDivide);
+    }
 }
--- meu-projeto/src/Exercicio01OO/Calculadora.java
+++ meu-projeto/src/Exercicio01OO/Calculadora.java
@@ -1,4 +1,21 @@
 package Exercicio01OO;
 
 public class Calculadora {
+
+    public int soma(int x, int y) {
+        return x + y;
+    }
+
+    public int subtrai(int x, int y) {
+        return x - y;
+    }
+
+    public int multiplica(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
 }
