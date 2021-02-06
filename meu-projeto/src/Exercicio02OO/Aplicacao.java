799ae8d09e64a177699e9530b968d1330912b651
thaisbg <thsbrgc@gmail.com>
2021-02-06 19:34:05 -0200

    Adicionando exercício 02 de OO

---
+++ meu-projeto/src/Exercicio02OO/Aplicacao.java
@@ -0,0 +1,9 @@
+package Exercicio02OO;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+
+        Comparador comparador = new Comparador();
+        System.out.println(comparador.menorQue(7,9));
+    }
+}
---
+++ meu-projeto/src/Exercicio02OO/Comparador.java
@@ -0,0 +1,12 @@
+package Exercicio02OO;
+
+public class Comparador {
+
+    public boolean menorQue(int x, int y) {
+        if (x < y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
