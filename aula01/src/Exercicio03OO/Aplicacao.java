121b1b4779a2c488dd7ac37d486a266189feac40
thaisbg <thsbrgc@gmail.com>
2021-02-11 11:26:23 -0200

    Reorganizando pastas

--- aula01/src/Exercicio03OO/Aplicacao.java
+++ aula01/src/Exercicio03OO/Aplicacao.java
@@ -3,7 +3,7 @@ package Exercicio03OO;
 public class Aplicacao {
 
     public static void main(String[] args) {
-        Aluno aluno = new Aluno("Iggy Pop", 9);
+        Aluno aluno = new Aluno("Iggy Pop", 4);
         System.out.println("Situação de aprovação: " + aluno.isAprovado());
     }
 }
